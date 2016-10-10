5afafdce044ed3e3546bf146f84d9900e2ce59e4
Markus Chiarot <cmarkus@hotmail.ch>
2016-10-10 19:51:02 +0200

    Section 8 - Chapter 43

--- src/app/stocks.component.ts
+++ src/app/stocks.component.ts
@@ -5,16 +5,50 @@ import {StockService} from './stock.service';
   selector: 'stocks',
   template: `<h3>Stocks</h3>
     {{title}}
-    <ul>
+    <ul [ngStyle]="{'color': mycolor, 'font-size': mysize}" [ngClass]="'centerClass'">
       <li *ngFor="let s of stocks">{{s}}</li>
     </ul>
-  `
+    
+    <hr>
+    
+    List of Stock Markets: <small>({{stockMarkets.length}} available)</small>
+    <ul *ngIf="stockMarkets.length >= 3" [ngClass]="{customClass:false, centerClass:iscenter}">
+      <li *ngFor="let sm of stockMarkets">{{sm}}</li>
+    </ul>
+    
+    <hr>
+    
+    <div [ngSwitch]="market" [ngClass]="'customClass'">
+      <div *ngSwitchCase="'NYSE'">New York Stock Exchange</div>
+      <div *ngSwitchCase="'LSE'">London Stock Exchange</div>
+      <div *ngSwitchCase="'HKSE'">Hong Kong Stock Exchange</div>
+      <div *ngSwitchCase="'NASDAQ'">National Association of Securities Dealers Automated Quotations</div>
+      <div *ngSwitchDefault>Could not find market: {{market}}</div>
+    </div>
+  `,
+  styles: [`
+    .customClass{
+      color: #0099ff;
+    }
+    
+    .centerClass{
+      text-align:center;
+    }
+  `]
 })
 
 export class StocksComponent{
 
   title = 'List of Stocks: ';
   stocks;
+
+  stockMarkets = ['NYSE', 'NASDAQ', 'EURONEXT', 'HKSE', 'LSE'];
+  showStockMarkets = true;
+  market = this.stockMarkets[2];
+
+  mycolor = '#ff9900';
+  mysize = '150%';
+  iscenter = true;
 
   constructor(ss : StockService){
     this.stocks = ss.getStocks();
